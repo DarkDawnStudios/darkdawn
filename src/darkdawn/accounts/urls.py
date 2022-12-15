4d5a1ca5dc9805b16c650b70f35d4dac63e6b302
Wang Zhao <wz@wangzhao.rocks>
2022-12-15 17:05:42 +0800

    minor updates

--- src/darkdawn/accounts/urls.py
+++ src/darkdawn/accounts/urls.py
@@ -5,5 +5,9 @@ from . import views
 app_name = "accounts"
 urlpatterns = [
     path("signup/", views.SignUpView.as_view(), name="signup"),
+    path("signup-weak/", views.signup_week_password, name="signup_weak"),
+    path("signin/", views.SignInView.as_view(), name="signin"),
     path("", views.index, name="index"),
+    path('logout/', views.logout, name='logout'),
+    path('recover/', views.recover, name='recover'),
 ]
